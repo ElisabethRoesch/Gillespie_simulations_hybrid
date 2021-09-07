02e52573b4ea9685e8dd55c2c63f07f3ae7931f9
elisabethroesch <eroesch@student.unimelb.edu.au>
2021-09-07 19:19:08 +1000

    error no method matching

--- BirthDeathProcess_VariableRateJump_ForwardDiff.jl
+++ BirthDeathProcess_VariableRateJump_ForwardDiff.jl
@@ -6,18 +6,21 @@ function f(du,u,p,t)
   du[1] = (p[1]-p[2])*u[1]+p[3]
 end
 prob = ODEProblem(f, [0.2], (0.0,10.0), [1.,0.1,0.1])
+sol = solve(prob, Tsit5())
+plot(sol)
 
 function diffs(x)
-  rate(u,p,t) = x[1]+u[1]
+  rate(u,p,t) = x[1]*u[1]
   affect!(integrator) = (integrator.u[1] = integrator.u[1]/2)
   jump = VariableRateJump(rate, affect!)
   jump_prob = JumpProblem(prob, Direct(), jump)
   sol = solve(jump_prob, Tsit5())
   return sol
 end
-
 x_1 = [1.]
-diffs(x_1)
+sol2 = diffs(x_1)
+plot(sol2)
+
 g = xx -> ForwardDiff.gradient(diffs, x_1); # g = ∇f
 g(x_1)
 
