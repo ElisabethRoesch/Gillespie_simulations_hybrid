30452544b1d7ac007b7ca0b15d97e858b50447fc
elisabethroesch <eroesch@student.unimelb.edu.au>
2021-10-02 08:27:08 +1000

    rerun wiht same time axis

--- Doc_progress/optimising/SDE_optim.jl
+++ Doc_progress/optimising/SDE_optim.jl
@@ -5,7 +5,7 @@ function f(du,u,p,t)
 end
 
 function g(du,u,p,t)
-  du[1] = 0.1*u[1]
+  du[1] = 0.3*u[1]
 end
 
 function diff_sde(p)
@@ -21,15 +21,15 @@ function diffs_sde(p)
   end
   ms = []
   for time_point_i in 1:length(sols[1])
-      sols_time_point_i = [sols[sol_iteration_j][time_point_i] for sol_iteration_j in 1:length(tt)]
+      sols_time_point_i = [sols[sol_iteration_j][time_point_i] for sol_iteration_j in 1:length(sols)]
       push!(ms, mean(sols_time_point_i))
   end
   return ms
 end
 
-p_1 = [1,15.]
+p_1 = [1,1.5]
 data = diff_sde(p_1)
-plot(data)
+scatter(data)
 
 function loss(p)
   if length(diffs_sde(p)) == length(data)
@@ -42,8 +42,7 @@ end
 opt_out = optimize(loss, [0.3, 0.4])
 res = Optim.minimizer(opt_out)
 
-prob = SDEProblem(f, g, [0.2], (0.0,10.0), res)
-sol = solve(prob, Tsit5(),saveat=0.4)
-d = [sol.u[i][1] for i in 1:length(sol)]
-plot(data, label = "Data")
-plot!(d, label = "Learnt")
+sol = diffs_sde(res)
+scatter(data, label = "Data")
+plot!(sol, label = "Learnt")
+savefig("Doc_progress/plots/SDE_optim.pdf")
