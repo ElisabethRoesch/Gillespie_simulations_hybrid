0236c746e036add1bdee5788b642cda224a30de2
elisabethroesch <eroesch@student.unimelb.edu.au>
2021-10-04 20:51:23 +1100

    prep Jump optim

--- Doc_progress/optim_gradient_free/ODE_optim.jl
+++ Doc_progress/optim_gradient_free/ODE_optim.jl
@@ -6,11 +6,11 @@ end
 
 function diff_ode(p)
   prob = ODEProblem(f, [0.2], (0.0,10.0), p)
-  sol = solve(prob, Tsit5(),saveat=0.4)
+  sol = solve(prob, Tsit5(),saveat=0.1)
   return [sol.u[i][1] for i in 1:length(sol)]
 end
 
-p_1 = [1.,1.5]
+p_1 = [1.,15.]
 data = diff_ode(p_1)
 
 function loss(p)
@@ -21,9 +21,7 @@ opt_out = optimize(loss, [1.3, 1.4])
 res = Optim.minimizer(opt_out)
 
 prob = ODEProblem(f, [0.2], (0.0,10.0), res)
-sol = solve(prob, Tsit5(),saveat=0.4)
+sol = solve(prob, Tsit5(),saveat=0.1)
 d = [sol.u[i][1] for i in 1:length(sol)]
-scatter(data)
+plot(data)
 plot!(d)
-
-savefig("Doc_progress/plots/ODE_optim.pdf")
---
+++ Doc_progress/optim_gradient_free/SDE_optim.jl
@@ -0,0 +1,48 @@
+using DifferentialEquations, Optim, Plots, Statistics
+
+function f(du,u,p,t)
+  du[1] = (p[1]-p[2])*u[1]
+end
+
+function g(du,u,p,t)
+  du[1] = 0.3*u[1]
+end
+
+function diff_sde(p)
+  prob = SDEProblem(f, g, [0.2], (0.0,10.0), p)
+  sol = solve(prob, Tsit5(),saveat=0.4)
+  return [sol.u[i][1] for i in 1:length(sol)]
+end
+function diffs_sde(p)
+  n_sols = 10
+  sols = []
+  for i in 1:n_sols
+    push!(sols, diff_sde(p))
+  end
+  ms = []
+  for time_point_i in 1:length(sols[1])
+      sols_time_point_i = [sols[sol_iteration_j][time_point_i] for sol_iteration_j in 1:length(sols)]
+      push!(ms, mean(sols_time_point_i))
+  end
+  return ms
+end
+
+p_1 = [1,1.5]
+data = diff_sde(p_1)
+scatter(data)
+
+function loss(p)
+  if length(diffs_sde(p)) == length(data)
+      return sum(abs2, diffs_sde(p) .- data)
+  else
+      return 100000
+  end
+end
+
+opt_out = optimize(loss, [0.3, 0.4])
+res = Optim.minimizer(opt_out)
+
+sol = diffs_sde(res)
+scatter(data, label = "Data")
+plot!(sol, label = "Learnt")
+savefig("Doc_progress/plots/SDE_optim.pdf")
