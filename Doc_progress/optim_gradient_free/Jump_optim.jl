9e34cd9c91adca4e51a7e02505ca6646b67ef3fc
elisabethroesch <eroesch@student.unimelb.edu.au>
2021-10-06 17:54:52 +1100

    test jump optim

---
+++ Doc_progress/optim_gradient_free/Jump_optim.jl
@@ -0,0 +1,54 @@
+using DifferentialEquations, DiffEqJump, Plots, Optim
+function diffs_variable(p)
+    function f(du,u,p,t)
+        du[1] = u[1]*0.0000001
+    end
+    brith_rate(u,p,t) = p[1]*u[1]
+    brith_affect!(integrator) = (integrator.u[1] = integrator.u[1]+1)
+    brith_jump = VariableRateJump(brith_rate, brith_affect!)
+    death_rate(u,p,t) = p[2]*u[1]
+    death_affect!(integrator) = (integrator.u[1] = integrator.u[1]-1)
+    death_jump = VariableRateJump(death_rate, death_affect!)
+    prob = ODEProblem(f, convert.(eltype(p), [0.2]), (0.0,10.0), p)
+    jump_prob = JumpProblem(prob, Direct(), brith_jump, death_jump)
+    sol = solve(jump_prob, Tsit5())(Array(range(0., stop = 10, step = 0.1))
+)
+    return sol
+end
+p_1 = [1., 1.5]
+sol_diffs = diffs_variable(p_1)
+plot(sol_diffs)
+
+sol_diffs(1.0)
+ts = Array(range(0., stop = 10, step = 0.1))
+data_raw = diffs_variable(p_1)
+data_vals = []
+for i in 1:101
+    v = data_raw.u[i][1]
+    push!(data_vals, v)
+end
+function loss(p)
+    sol_j = diffs_variable(p)
+    vals = []
+    for i in 1:101
+        v = sol_j.u[i][1]
+        push!(vals, v)
+    end
+  return sum(abs2, vals .- data_vals)
+end
+
+opt_out = optimize(loss, [1.3, 1.4], iterations = 1000)
+res = Optim.minimizer(opt_out)
+sol_res = diffs_variable(res)
+
+vals = []
+for i in 1:101
+    v = sol_res.u[i][1]
+    push!(vals, v)
+end
+
+plot(vals)
+plot!(vals)
+
+
+savefig("Doc_progress/plots/optim/Jump_case.pdf")
